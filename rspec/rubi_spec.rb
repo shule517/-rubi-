d3fd82cebb033813e9eaf9200ebed669948a7d06
shule517 <shule517@gmail.com>
2025-05-02 09:49:03 +0900

    parse_lispを修正した

--- rspec/rubi_spec.rb
+++ rspec/rubi_spec.rb
@@ -1,29 +1,36 @@
 require_relative '../rubi.rb'
 
-describe '#split_tokens' do
-  subject { split_tokens(str) }
+describe 'rubi.rb' do
+  describe '#split_tokens' do
+    subject { split_tokens(str) }
 
-  context '足し算' do
-    let(:str) { "(+ 1 2)" }
-    it { is_expected.to eq %w[( + 1 2 )] }
+    context '足し算' do
+      let(:str) { "(+ 1 2)" }
+      it { is_expected.to eq [:"(", :"+", 1, 2, :")"] }
+    end
+
+    context '二重かっこ' do
+      let(:str) { "(defun double (x) (* x 2))" }
+      it { is_expected.to eq [:"(", :defun, :double, :"(", :x, :")", :"(", :*, :x, 2, :")", :")"] }
+    end
   end
 
-  context '二重かっこ' do
-    let(:str) { "(defun double (x) (* x 2))" }
-    it { is_expected.to eq %w[( defun double ( x ) ( * x 2 ) )] }
+  describe '#parse_lisp' do
+    subject { parse_lisp(split_tokens(str)) }
+
+    context '()*1' do
+      let(:str) { "(+ 1 2)" }
+      it { is_expected.to eq [:+, 1, 2] }
+    end
+
+    context '()*2' do
+      let(:str) { "(+ (+ 1 2) 3)" }
+      it { is_expected.to eq [:+, [:+, 1, 2], 3] }
+    end
+
+    context '二重かっこ' do
+      let(:str) { "(defun double (x) (* x 2))" }
+      it { is_expected.to eq [:defun, :double, [:x], [:*, :x, 2]] }
+    end
   end
-end
-
-describe '#parse_lisp' do
-  subject { parse_lisp(str) }
-
-  context '足し算' do
-    let(:str) { "(+ 1 2)" }
-    it { is_expected.to eq [:+, 1, 2] }
-  end
-
-  # context '二重かっこ' do
-  #   let(:str) { "(defun double (x) (* x 2))" }
-  #   it { is_expected.to eq %w[( defun double ( x ) ( * x 2 ) )] }
-  # end
 end
